700c6abd56a7c68cf4dc8d51df4516b5886b9a16
Asaf David <asaf.david@allcloud.io>
2019-12-27 01:08:41 +0200

    test

--- providers.tf
+++ providers.tf
@@ -4,13 +4,14 @@ terraform {
         key            = "workspacebased/ec2.tfstate"
         region         = "us-east-1"
         dynamodb_table = "terraform_locks"
-        role_arn       = "arn:aws:iam::300301835588:role/eksctl-learn-eks-addon-iamserviceaccount-kub-Role1-HBDXIKKQU5ED"
+        role_arn       = "arn:aws:iam::300301835588:role/atlantis"
     }
 }
 
 provider "aws" {
   region = var.aws_region
   assume_role {
-    role_arn       = "arn:aws:iam::300301835588:role/eksctl-learn-eks-addon-iamserviceaccount-kub-Role1-HBDXIKKQU5ED"
+    role_arn       = "arn:aws:iam::300301835588:role/atlantis"
+
   }
 }
