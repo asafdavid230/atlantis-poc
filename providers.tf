7a771a793592b68755506df0dd9b496e3052a9a7
Asaf David <asaf.david@allcloud.io>
2019-12-26 22:23:11 +0200

    update assume role

--- providers.tf
+++ providers.tf
@@ -4,13 +4,13 @@ terraform {
         key            = "workspacebased/ec2.tfstate"
         region         = "us-east-1"
         dynamodb_table = "terraform_locks"
-        role_arn       = "arn:aws:iam::300301835588:role/atlantis-role"
+        role_arn       = "arn:aws:iam::300301835588:role/eksctl-learn-eks-addon-iamserviceaccount-kub-Role1-HBDXIKKQU5ED"
     }
 }
 
 provider "aws" {
   region = var.aws_region
   assume_role {
-    role_arn       = "arn:aws:iam::300301835588:role/atlantis-role"
+    role_arn       = "arn:aws:iam::300301835588:role/eksctl-learn-eks-addon-iamserviceaccount-kub-Role1-HBDXIKKQU5ED"
   }
 }
